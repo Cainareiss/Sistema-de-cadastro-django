dda85c81c601a38eed81f62fb90c779998d04b9c
Cainã Reis <106223762+Cainareiss@users.noreply.github.com>
2024-03-22 03:46:00 -0300

    Add files via upload

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseBadRequest
 from .models import Usuario
 
+from django.shortcuts import render, redirect
+from django.http import HttpResponseBadRequest
+from .models import Usuario
 
 def home(request):
     return render(request, 'home.html')
@@ -8,16 +12,28 @@ def home(request):
 def usuarios(request):
     if request.method == 'POST':
         # Salvar os dados da tela para o banco de dados.
-        novo_usuario = Usuario()  # Aqui é instanciado um novo objeto da classe Usuario
+        novo_usuario = Usuario()
         novo_usuario.nome = request.POST.get('nome')
         novo_usuario.idade = request.POST.get('idade')
         novo_usuario.save()
 
-    # Exibir todos os usuários já cadastrados em uma nova página
-    usuarios = {
-        'usuarios': Usuario.objects.all()  # Aqui é recuperada a lista de todos os usuários cadastrados
-    }
+    # Recuperar a lista de usuários cadastrados
+    usuarios = Usuario.objects.all()
 
-    # Retornar os dados para a página de listagem de usuários
-    return render(request, 'usuarios.html', usuarios)
+    # Verificar se os IDs dos usuários são válidos
+    for usuario in usuarios:
+        if not usuario.id:
+            usuario.delete()  # Excluir usuário sem ID
+            return HttpResponseBadRequest("Um ou mais usuários sem ID foram removidos.")
     
+    # Passar a lista de usuários para o template
+    return render(request, 'usuarios.html', {'usuarios': usuarios})
+
+def remover_usuarios(request, id):
+    if request.method == 'POST':
+        usuario = Usuario.objects.get(id=id)
+        usuario.delete()
+        return redirect('listagem_usuarios')
+    else:
+        # Se o método não for POST, talvez você queira retornar um erro ou redirecionar para outra página
+        return HttpResponseBadRequest("Método não permitido")
--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -1,11 +1,12 @@
-
 from django.urls import path
 from app_cad_usuarios import views
 
 urlpatterns = [
    # rota, view responsavel, nome de referencia
    # usuarios.com
-   path('',views.home,name='home'),
+   path('', views.home, name='home'),
    # usuarios.com/usuarios
-   path('usuarios/',views.usuarios,name='listagem_usuarios')
+   path('usuarios/', views.usuarios, name='listagem_usuarios'),
+   # Esta rota será para remover um usuário específico, então vamos adicionar um parâmetro 'id' para diferenciá-la
+   path('usuarios/remover/<int:id>/', views.remover_usuarios, name='remover_usuarios')
 ]
